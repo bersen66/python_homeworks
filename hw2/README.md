631c0bad0ad42516c06cadc1dc9ebe86598ff2ba
ortur <orturb@yandex.ru>
2024-12-25 19:25:30 +0300

    Done: hw2

---
+++ hw2/README.md
@@ -0,0 +1,7 @@
+# Как получить артефакты: (хранятся в папке artifacts)
+
+Соберет docker-image и с их помощью сгенерирует файлы `from_docker.tex` и `from_docker.pdf`
+
+```
+make gen_all
+```
--- hw2/app.py
+++ hw2/app.py
@@ -1,20 +1,36 @@
+#!/usr/bin/python
+
 from mytex import *
+import argparse
+
+def main(image_dir, image_name, output_file):
+    TableBody = [
+        (1, 2),
+        ("Hello", "world"),
+        ("Floppa", "the big cat")
+    ]
+
+    table = make_table(with_centering(with_caption("MyTable", with_tabular("|c|c|", with_hline(table_body)))))
+
+    doc = complex_document(
+        enable_images(image_dir),
+        inclue_graphics(image_name).then(table, TableBody)
+    )
 
 
-TableBody = [
-    (1, 2),
-    ("Hello", "world"),
-    ("Floppa", "the big cat")
-]
-
-table = make_table(with_centering(with_caption("MyTable", with_tabular("|c|c|", with_hline(table_body)))))
-
-doc = complex_document(
-    enable_images("./images/"),
-    inclue_graphics("image.png").then(table, TableBody)
-)
+    with open(output_file, 'w') as file:
+        for line in doc:
+            file.writelines(f"{line}\n")
 
 
-with open("artifacts/doc.tex", 'w') as file:
-    for line in doc:
-        file.writelines(f"{line}\n")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate a LaTeX document with an image and a table.')
+    parser.add_argument('image_dir', type=str, help='Directory containing the image.')
+    parser.add_argument('image_name', type=str, help='Name of the image file (including extension).')
+    parser.add_argument('output_file', type=str, help='Name of the output LaTeX file.')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Call the main function with parsed arguments
+    main(args.image_dir, args.image_name, args.output_file)
